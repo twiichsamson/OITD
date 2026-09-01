b406868818d7765ddec89cdfa3390c8d6111295d
Tewahido Samson <tewahido.samson@bdo-ea.com>
2026-09-01 10:39:03 +0300

    created estate folder and init and manifest py files

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Real Estate",
+    "version": "19.0.0",
+    "depends": ["base"],
+    "application": True,
+    "depends": ["base"],
+    "data": {
+
+    },
+    "installation": True,
+    "License": "LGPL-3",
+
+}
