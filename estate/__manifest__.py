825345792c695efb6d6f949b581064ac63742b9c
Tewahido Samson <tewahido.samson@bdo-ea.com>
2026-09-01 14:04:06 +0300

    created view folders and menues and properties

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Real Estate",
-    "version": "19.0.1",
+    "version": "19.0.2",
     "application": True,
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
     "installable": True,
     "license": "LGPL-3",
